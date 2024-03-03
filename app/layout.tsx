be0b7c2bbdd640da6ffee03bc4b2123cdab55c1b
Tyler Landtroop <98730695+tlandtroop@users.noreply.github.com>
2024-03-02 20:02:36 -0500

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "react-hot-toast";
 
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Footer />
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
       </body>
